62037310d73f6b8a7582c359b1899884383bc449
monsever13 <avanza_cs1@rocketmail.com>
2020-11-14 21:51:23 +0700

    menambahkan beberapa revisi

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,28 +4,38 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="row">
-          <div>
-            <a className="brand" href="/">
-              amazona
-            </a>
-          </div>
-          <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
-          </div>
-        </header>
-        <main>
-          <Route path="/product/:id" component={ProductScreen}></Route>
-          <Route path="/" component={HomeScreen} exact></Route>
-        </main>
-        <footer className="row center">All right reserved</footer>
-      </div>
-    </BrowserRouter>
-  );
+    return ( <
+        BrowserRouter >
+        <
+        div className = "grid-container" >
+        <
+        header className = "row" >
+        <
+        div >
+        <
+        a className = "brand"
+        href = "/" >
+        Bilqis Store <
+        /a> <
+        /div> <
+        div >
+        <
+        a href = "/cart" > Cart < /a> <
+        a href = "/signin" > Sign In < /a> <
+        /div> <
+        /header> <
+        main >
+        <
+        Route path = "/product/:id"
+        component = { ProductScreen } > < /Route> <
+        Route path = "/"
+        component = { HomeScreen }
+        exact > < /Route> <
+        /main> <
+        footer className = "row center" > All right reserved < /footer> <
+        /div> <
+        /BrowserRouter>
+    );
 }
 
 export default App;
