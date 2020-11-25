6fd90762cfbe35bc3bc547cd29c0b1799e561075
monsever13 <avanza_cs1@rocketmail.com>
2020-11-25 17:05:57 +0700

    order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ function App() {
         <header className="row">
           <div>
             <Link className="brand" to="/">
-              amazona
+              Bilqis
             </Link>
           </div>
           <div>
