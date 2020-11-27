5a8548e8aa089acce935835e73590a8b66fccd14
monsever13 <avanza_cs1@rocketmail.com>
2020-11-27 15:21:39 +0700

    34 update user profile

--- backend/data.js
+++ backend/data.js
@@ -3,16 +3,10 @@ import bcrypt from 'bcryptjs';
 const data = {
   users: [
     {
-      name: 'Basir',
-      email: 'admin@example.com',
+      name: 'Bilqis',
+      email: 'avanzacs1@gmail.com',
       password: bcrypt.hashSync('1234', 8),
       isAdmin: true,
-    },
-    {
-      name: 'John',
-      email: 'user@example.com',
-      password: bcrypt.hashSync('1234', 8),
-      isAdmin: false,
     },
   ],
   products: [
